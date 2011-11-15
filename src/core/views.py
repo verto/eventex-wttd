53bd221401b3bf56de9edfd2e4aa006614a6c424
Everton Cardoso <ercardoso@gmail.com>
2011-11-15 11:09:45 -0300

    defining style to hotsite

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def homepage(request):
-	return render_to_response('index.html')
+	context = RequestContext(request)
+	return render_to_response('index.html',context)
+	
